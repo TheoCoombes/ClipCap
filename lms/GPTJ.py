e96bd7ecbbb5ec249a6fc21343cad861a4108a6c
TheoCoombes <theocoombes06@gmail.com>
2022-01-27 20:27:10 +0000

    fix

--- lms/GPTJ.py
+++ lms/GPTJ.py
@@ -126,27 +126,27 @@ def convert_to_int8(model):
                     )
                 )
 
-class GPTJBlock(transformers.models.gptj.modeling_gptj.GPTJBlock):
-    def __init__(self, config):
-        super().__init__(config)
+# class GPTJBlock(transformers.models.gptj.modeling_gptj.GPTJBlock):
+#     def __init__(self, config):
+#         super().__init__(config)
 
-        convert_to_int8(self.attn)
-        convert_to_int8(self.mlp)
+#         convert_to_int8(self.attn)
+#         convert_to_int8(self.mlp)
 
 
-class GPTJModel(transformers.models.gptj.modeling_gptj.GPTJModel):
-    def __init__(self, config):
-        super().__init__(config)
-        convert_to_int8(self)
+# class GPTJModel(transformers.models.gptj.modeling_gptj.GPTJModel):
+#     def __init__(self, config):
+#         super().__init__(config)
+#         convert_to_int8(self)
         
 
-class GPTJForCausalLM(transformers.models.gptj.modeling_gptj.GPTJForCausalLM):
-    def __init__(self, config):
-        super().__init__(config)
-        convert_to_int8(self)
+# class GPTJForCausalLM(transformers.models.gptj.modeling_gptj.GPTJForCausalLM):
+#     def __init__(self, config):
+#         super().__init__(config)
+#         convert_to_int8(self)
 
 
-transformers.models.gptj.modeling_gptj.GPTJBlock = GPTJBlock  # monkey-patch GPT-J
+# transformers.models.gptj.modeling_gptj.GPTJBlock = GPTJBlock  # monkey-patch GPT-J
 
 
 class GPTJ(GPTJForCausalLM):
