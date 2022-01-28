d0cb6581760a5753db73665ef4c86201148f5b9b
TheoCoombes <theocoombes06@gmail.com>
2022-01-28 17:00:50 +0000

    allow multiple captions to be generated in inference

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 from torchvision.transforms import Compose
 import torch.nn.functional as nnf
+from typing import Tuple, List
 from clip.model import CLIP
 from typing import Union
 import skimage.io as io
@@ -18,6 +19,7 @@ def generate_beam(
     model: Union[CLIPCaptionModel, CLIPCaptionPrefixOnly],
     tokenizer: GPT2_Tokenizer,
     embed: torch.Tensor,
+    number_to_generate: int = 1,
     beam_size: int = 5,
     entry_length: int = 67,
     temperature: float = 1.0,
@@ -32,65 +34,71 @@ def generate_beam(
     seq_lengths = torch.ones(beam_size, device="cuda:7")
     has_stopped = torch.zeros(beam_size, dtype=torch.bool, device="cuda:7")
 
+    generations = []
+
     with torch.no_grad():
-        for _ in range(entry_length):
-            outputs = model.language_model.call(inputs_embeds=embed)
-            logits = outputs.logits
-            logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
-            logits = logits.softmax(-1).log()
+        for i in range(number_to_generate):
+            for _ in range(entry_length):
+                outputs = model.language_model.call(inputs_embeds=embed)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                logits = logits.softmax(-1).log()
 
-            if scores is None:
-                scores, next_tokens = logits.topk(beam_size, -1)
-                embed = embed.expand(beam_size, *embed.shape[1:])
-                next_tokens, scores = next_tokens.permute(1, 0), scores.squeeze(0)
-                if tokens is None:
-                    tokens = next_tokens
+                if scores is None:
+                    scores, next_tokens = logits.topk(beam_size, -1)
+                    embed = embed.expand(beam_size, *embed.shape[1:])
+                    next_tokens, scores = next_tokens.permute(1, 0), scores.squeeze(0)
+                    if tokens is None:
+                        tokens = next_tokens
+                    else:
+                        tokens = tokens.expand(beam_size, *tokens.shape[1:])
+                        tokens = torch.cat((tokens, next_tokens), dim=1)
                 else:
-                    tokens = tokens.expand(beam_size, *tokens.shape[1:])
+                    logits[has_stopped] = -float(np.inf)
+                    logits[has_stopped, 0] = 0
+
+                    scores_sum = scores[:, None] + logits
+                    seq_lengths[~has_stopped] += 1
+                    scores_sum_average = scores_sum / seq_lengths[:, None]
+                    scores_sum_average, next_tokens = scores_sum_average.view(-1).topk(beam_size, -1)
+
+                    next_tokens_source = next_tokens // scores_sum.shape[1]
+                    seq_lengths = seq_lengths[next_tokens_source]
+                    next_tokens = next_tokens % scores_sum.shape[1]
+                    next_tokens = next_tokens.unsqueeze(1)
+
+                    tokens = tokens[next_tokens_source]
                     tokens = torch.cat((tokens, next_tokens), dim=1)
-            else:
-                logits[has_stopped] = -float(np.inf)
-                logits[has_stopped, 0] = 0
 
-                scores_sum = scores[:, None] + logits
-                seq_lengths[~has_stopped] += 1
-                scores_sum_average = scores_sum / seq_lengths[:, None]
-                scores_sum_average, next_tokens = scores_sum_average.view(-1).topk(beam_size, -1)
+                    embed = embed[next_tokens_source]
+                    scores = scores_sum_average * seq_lengths
 
-                next_tokens_source = next_tokens // scores_sum.shape[1]
-                seq_lengths = seq_lengths[next_tokens_source]
-                next_tokens = next_tokens % scores_sum.shape[1]
-                next_tokens = next_tokens.unsqueeze(1)
-
-                tokens = tokens[next_tokens_source]
-                tokens = torch.cat((tokens, next_tokens), dim=1)
-
-                embed = embed[next_tokens_source]
-                scores = scores_sum_average * seq_lengths
-
-                has_stopped = has_stopped[next_tokens_source]
-            
-            next_token_embed = model.language_model.get_embedding_text(next_tokens.squeeze()).view(embed.shape[0], 1, -1)
-            embed = torch.cat((embed, next_token_embed), dim=1)
-            has_stopped = has_stopped + next_tokens.eq(stop_token).squeeze()
-
-            if has_stopped.all():
-                break
+                    has_stopped = has_stopped[next_tokens_source]
                 
-    scores = scores / seq_lengths
-    output_list = tokens.cpu().numpy()
-    output_texts = [tokenizer.decode_tokens(output[:int(length)]) for output, length in zip(output_list, seq_lengths)]
+                next_token_embed = model.language_model.get_embedding_text(next_tokens.squeeze()).view(embed.shape[0], 1, -1)
+                embed = torch.cat((embed, next_token_embed), dim=1)
+                has_stopped = has_stopped + next_tokens.eq(stop_token).squeeze()
 
-    order = scores.argsort(descending=True)
-    output_texts = [output_texts[i] for i in order]
+                if has_stopped.all():
+                    break
+                
+            scores = scores / seq_lengths
+            output_list = tokens.cpu().numpy()
+            output_texts = [tokenizer.decode_tokens(output[:int(length)]) for output, length in zip(output_list, seq_lengths)]
 
-    return output_texts
+            order = scores.argsort(descending=True)
+            output_texts = [output_texts[i] for i in order]
+
+            generations.append(output_texts)
+    
+    return generations
 
 
 def generate_no_beam(
     model: Union[CLIPCaptionModel, CLIPCaptionPrefixOnly],
     tokenizer: GPT2_Tokenizer,
     embeds: torch.Tensor,
+    number_to_generate: int = 1,
     entry_length: int = 67,
     top_p: float = 0.8,
     temperature: float = 1.0,
@@ -101,52 +109,57 @@ def generate_no_beam(
     tokens = None
 
     filter_value = -float(np.inf)
+    generations = []
 
     with torch.no_grad():
-        for _ in range(entry_length):
-            outputs = model.language_model.call(inputs_embeds=embeds)
-            logits = outputs.logits
-            logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+        for i in range(number_to_generate):
+            for _ in range(entry_length):
+                outputs = model.language_model.call(inputs_embeds=embeds)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
 
-            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-            cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
 
-            sorted_indices_to_remove = cumulative_probs > top_p
-            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-            sorted_indices_to_remove[..., 0] = 0
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+                sorted_indices_to_remove[..., 0] = 0
 
-            indices_to_remove = sorted_indices[sorted_indices_to_remove]
-            logits[:, indices_to_remove] = filter_value
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
 
-            next_token = torch.argmax(logits, -1).unsqueeze(0)
-            next_token_embed = model.language_model.get_embedding_text(next_token)
+                next_token = torch.argmax(logits, -1).unsqueeze(0)
+                next_token_embed = model.language_model.get_embedding_text(next_token)
 
-            if tokens is None:
-                tokens = next_token
-            else:
-                tokens = torch.cat((tokens, next_token), dim=1)
-            
-            embeds = torch.cat((embeds, next_token_embed), dim=1)
-            
-            if stop_token == next_token.item():
-                break
+                if tokens is None:
+                    tokens = next_token
+                else:
+                    tokens = torch.cat((tokens, next_token), dim=1)
+                
+                embeds = torch.cat((embeds, next_token_embed), dim=1)
+                
+                if stop_token == next_token.item():
+                    break
 
-        output_list = list(tokens.squeeze().cpu().numpy())
-        output_text = tokenizer.decode_tokens(output_list)
+            output_list = list(tokens.squeeze().cpu().numpy())
+            output_text = tokenizer.decode_tokens(output_list)
+        
+            generations.append(output_text)
     
-    return output_text
+    return generations
 
 
-def demo_generate_caption(
+def demo_generate_captions(
     model: Union[CLIPCaptionModel, CLIPCaptionPrefixOnly],
     tokenizer: GPT2_Tokenizer,
     clip_model: CLIP,
     clip_preproc: Compose,
     image: Image.Image,
+    number_to_generate: int = 1,
     use_beam_search: bool = False,
     device: str = "cuda:0",
     **generation_kwargs
-) -> str:
+) -> Tuple[List[str], torch.Tensor]:
     image = clip_preproc(image).unsqueeze(0).to(device)
 
     with torch.no_grad():
@@ -154,11 +167,13 @@ def demo_generate_caption(
         prefix_embed = model.clip_project(prefix).reshape(1, 40, -1)
     
     if use_beam_search:
-        generated_caption = generate_beam(model, tokenizer, prefix_embed, **generation_kwargs)
+        generated_captions = generate_beam(model, tokenizer, prefix_embed,
+            number_to_generate=number_to_generate, **generation_kwargs)
     else:
-        generated_caption = generate_no_beam(model, tokenizer, prefix_embed, **generation_kwargs)
+        generated_captions = generate_no_beam(model, tokenizer, prefix_embed,
+            number_to_generate=number_to_generate, **generation_kwargs)
     
-    return generated_caption, prefix
+    return generated_captions, prefix
 
 
 def demo(
@@ -216,7 +231,7 @@ def demo(
             image = io.imread(image_path_url)
             image = Image.fromarray(image)
 
-            caption = demo_generate_caption(
+            caption = demo_generate_captions(
                 model, tokenizer, clip_model, preprocess, image,
                 use_beam_search=use_beam_search, device=device, **generation_kwargs
             )
@@ -231,6 +246,7 @@ def demo(
 def _shutterstock_demo(
     checkpoint_path: str,
     shutterstock_path: str,
+    number_to_generate: int = 1,
     device: str = "cuda:0",
     use_beam_search: bool = True,
     out_filename_prefix: str = "demo_inference",
@@ -259,17 +275,15 @@ def _shutterstock_demo(
         with open(metadata_file, "r") as f:
             metadata = json.load(f)
 
-        caption, image_features = demo_generate_caption(
+        captions, image_features = demo_generate_captions(
             model, tokenizer, clip_model, preprocess, pil_image,
-            use_beam_search=use_beam_search, device=device
+            use_beam_search=use_beam_search, device=device, number_to_generate=number_to_generate
         )
 
         url = metadata["src"]
         original_caption = metadata["alt"]
 
-        caption = caption[0]
-
-        text_inputs = clip.tokenize([caption, original_caption], truncate=True).to(device)
+        text_inputs = clip.tokenize([original_caption, *captions], truncate=True).to(device)
 
         with torch.no_grad():
             text_features = clip_model.encode_text(text_inputs)
@@ -278,15 +292,21 @@ def _shutterstock_demo(
         text_features /= text_features.norm(dim=-1, keepdim=True)
 
         similarities = image_features.cpu().numpy() @ text_features.cpu().numpy().T
+        similarities = similarities.tolist()
 
-        generated_sim, original_sim = similarities[0]
-        
+        original_sim = similarities[0][0]
+        generated_sims = similarities[0][:1]
+
+        best_sim = max(generated_sims)
+        best_caption = captions[generated_sims.index(best_sim)]
 
         sample_data[url] = {
             "original_caption": original_caption,
-            "original_sim": float(original_sim),
-            "generated_caption": caption,
-            "generated_sim": float(generated_sim)
+            "original_sim": original_sim,
+            "generated_captions": captions,
+            "generated_sim": generated_sims,
+            "best_caption": best_caption,
+            "best_sim": best_sim
         }
     
     with open(f"{out_filename_prefix}_shutterstock.json", "w+") as f:
