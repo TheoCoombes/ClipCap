a5960d1e3d6739bb700d0350c39df91a377acad7
TheoCoombes <theocoombes06@gmail.com>
2022-05-30 14:56:37 +0100

    fix

--- clipcap/encoders/args.py
+++ clipcap/encoders/args.py
@@ -12,7 +12,7 @@ def add_encoder_args(parser: ArgumentParser) -> ArgumentParser:
     encoder.add_argument(
         "--encoder-model-variant",
         type=str,
-        default="ViT-L/14",
+        default="ViT-L//14",
         help="The specific version of CLIP e.g. 'ViT-L/14', or the path to the CLAP checkpoint.",
     )
     encoder.add_argument(
