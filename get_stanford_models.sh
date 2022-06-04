08a046432fa00d8eadd56990bd50ba151c4375fb
TheoCoombes <theocoombes06@gmail.com>
2022-06-04 01:25:16 +0100

    fix

--- get_stanford_models.sh
+++ get_stanford_models.sh
@@ -2,7 +2,7 @@
 # This script downloads the Stanford CoreNLP models for eval.
 
 CORENLP=stanford-corenlp-full-2015-12-09
-SPICELIB=pycocoevalcap/spice/lib
+SPICELIB=clipcap/eval/pycocoevalcap/spice/lib
 JAR=stanford-corenlp-3.6.0
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
