81f44e8e565e99a351f8c6bc0ccb77f54a6f20ab
TheoCoombes <theocoombes06@gmail.com>
2022-06-11 13:56:36 +0100

    test a fix for eval

--- clipcap/eval/base.py
+++ clipcap/eval/base.py
@@ -6,7 +6,7 @@ from clipcap.model.load import load
 
 from clipcap.eval.args import add_eval_args
 from clipcap.eval.dataset import EvalDataset
-from clipcap.eval.metrics import evaluate_metrics
+from clipcap.eval.metrics import evaluate_metrics_from_lists
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace
 from typing import Tuple, Union, Callable
@@ -18,10 +18,6 @@ import numpy as np
 import torch
 import json
 import tqdm
-
-_DEFAULT_REFERENCE_CAPTION_COLUMNS = "caption_reference_{{01..{:02d}}}"
-_DEFAULT_PREDICTION_COLUMN = "caption_predicted"
-_DEFAULT_FILENAME_COLUMN = "file_name"
 
 def prepare_model(args: Namespace) -> Tuple[Union[ClipCapModel, ClipCapModelPrefixOnly], Callable, Callable, Callable]:
     model, tokenizer = load(
@@ -36,68 +32,67 @@ def prepare_model(args: Namespace) -> Tuple[Union[ClipCapModel, ClipCapModelPref
     return model, tokenizer, encode_method, sample_processor
 
 def eval(args: Namespace) -> int:
-    model, tokenizer, encode_method, sample_processor = prepare_model(args)
+    # model, tokenizer, encode_method, sample_processor = prepare_model(args)
 
-    dataset = EvalDataset(sample_processor, args.sample_path)
-    predictions = []
+    # dataset = EvalDataset(sample_processor, args.sample_path)
+    # predictions = {}
 
-    for filename, sample in tqdm.tqdm(dataset, desc="eval"):
-        sample = sample.unsqueeze(0).to(args.device)
+    # for filename, sample in tqdm.tqdm(dataset, desc="eval"):
+    #     sample = sample.unsqueeze(0).to(args.device)
     
-        with torch.no_grad():
-            media_features = encode_method(sample)
-            prefix = model.transformer_mapper(media_features)
+    #     with torch.no_grad():
+    #         media_features = encode_method(sample)
+    #         prefix = model.transformer_mapper(media_features)
 
-        captions = generate_nucleus_sampling(
-            model, tokenizer, prefix,
-            number_to_generate=args.number_to_generate,
-            text_prefix_tokens=None,
-            top_p=args.top_p,
-            top_k=args.top_k,
-            temperature=args.temperature,
-            # repetition_penalty=args.repetition_penalty,
-            # desired_sentence_length=args.desired_sentence_length,
-        )
+    #     captions = generate_nucleus_sampling(
+    #         model, tokenizer, prefix,
+    #         number_to_generate=args.number_to_generate,
+    #         text_prefix_tokens=None,
+    #         top_p=args.top_p,
+    #         top_k=args.top_k,
+    #         temperature=args.temperature,
+    #         # repetition_penalty=args.repetition_penalty,
+    #         # desired_sentence_length=args.desired_sentence_length,
+    #     )
 
-        caption_tokens = tokenize(captions).to(args.device)
+    #     caption_tokens = tokenize(captions).to(args.device)
 
-        with torch.no_grad():
-            if model.config.encoder_config.use_windowed_embeddings:
-                media_features = media_features[0][0].unsqueeze(0)
+    #     with torch.no_grad():
+    #         if model.config.encoder_config.use_windowed_embeddings:
+    #             media_features = media_features[0][0].unsqueeze(0)
             
-            media_features, text_features, media_features_mlp, text_features_mlp, logit_scale_a, logit_scale_t = encode_method.model(sample, caption_tokens)
+    #         media_features, text_features, media_features_mlp, text_features_mlp, logit_scale_a, logit_scale_t = encode_method.model(sample, caption_tokens)
             
-            text_features /= text_features.norm(dim=-1, keepdim=True)
-            media_features /= media_features.norm(dim=-1, keepdim=True)
+    #         text_features /= text_features.norm(dim=-1, keepdim=True)
+    #         media_features /= media_features.norm(dim=-1, keepdim=True)
 
-            a_logits_per_audio = (logit_scale_a * media_features @ text_features_mlp.t()).detach().cpu()
-            t_logits_per_audio = (logit_scale_t * media_features_mlp @ text_features.t()).detach().cpu()
+    #         a_logits_per_audio = (logit_scale_a * media_features @ text_features_mlp.t()).detach().cpu()
+    #         t_logits_per_audio = (logit_scale_t * media_features_mlp @ text_features.t()).detach().cpu()
 
-            similarities = ((a_logits_per_audio + t_logits_per_audio) / 2)[0].numpy()
+    #         similarities = ((a_logits_per_audio + t_logits_per_audio) / 2)[0].numpy()
         
-        best_idx = int(np.argmax(similarities))
-        caption = captions[best_idx]
+    #     best_idx = int(np.argmax(similarities))
+    #     caption = captions[best_idx]
         
-        predictions.append({
-            _DEFAULT_FILENAME_COLUMN: filename,
-            _DEFAULT_PREDICTION_COLUMN: caption
-        })
+    #     predictions[filename] = caption
     
-    with open("eval.json", "w+") as f:
-        json.dump(predictions, f)
+    with open("eval2.json", "r") as f:
+        predictions = json.load(f)
     
-    col_list = [args.csv_filename_column, *braceexpand(args.csv_reference_caption_columns)]
-    reference = pd.read_csv(args.reference_csv, usecols=col_list)
+    reference_df = pd.read_csv(args.reference_csv)
+    predictions_list = []
+    references_list = []
+    ids = []
 
-    number_reference_captions = len(col_list) - 1
-    new_brace = _DEFAULT_REFERENCE_CAPTION_COLUMNS.format(number_reference_captions)
-    new_col_list = [_DEFAULT_FILENAME_COLUMN, *braceexpand(new_brace)]
+    for index, row in reference_df.iterrows():
+        filename = row[args.csv_filename_column]
+        references = [row[key] for key in braceexpand(args.csv_reference_caption_columns)]
+        references_list.append(references)
+        predictions_list.append(predictions[filename])
+        ids.append(index)
 
-    reference = reference.rename(columns={old: new for old, new in zip(col_list, new_col_list)})
-    reference = reference.to_dict('records')
-
-    scores = evaluate_metrics(
-        predictions, reference, nb_reference_captions=number_reference_captions
+    scores = evaluate_metrics_from_lists(
+        predictions_list, references_list, ids
     )
 
     if args.save_file is not None:
