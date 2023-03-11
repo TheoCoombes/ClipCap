083c58ff9f0727778e5e37508404d703f5fd6572
Theo Coombes <theocoombes06@gmail.com>
2023-03-11 19:21:25 +0000

    new clap model

--- clipcap/encoders/args.py
+++ clipcap/encoders/args.py
@@ -13,7 +13,7 @@ def add_encoder_args(parser: ArgumentParser) -> ArgumentParser:
         "--encoder-model-variant",
         type=str,
         default="ViT-L_14",
-        help="The specific version of CLIP e.g. 'ViT-L_14' ('_' gets replaced with a forward slash), or the path to the CLAP checkpoint.",
+        help="The specific version of CLIP e.g. 'ViT-L_14' ('_' gets replaced with a forward slash), ignore for CLAP.",
     )
     encoder.add_argument(
         "--normalize-embeddings",
@@ -27,19 +27,19 @@ def add_encoder_args(parser: ArgumentParser) -> ArgumentParser:
         "--use-windowed-embeddings",
         type=bool,
         default=False,
-        help="Transforms the data into 'windows' so that more embeddings can be generated without needing fine-grained models.",
+        help="Transforms the data into 'windows' so that more embeddings can be generated without needing fine-grained models. [IGNORE FOR CLAP]",
     )
     windowed.add_argument(
         "--window-size",
         type=int,
         default=(4 * 4),
-        help="If enabled: In CLIP, this is the number of tiles to split the image into (e.g. 3x3 = 9). In CLAP, this should be the number of splices the audio recieves before being encoded.",
+        help="If enabled: In CLIP, this is the number of tiles to split the image into (e.g. 3x3 = 9). In CLAP, this should be the number of splices the audio recieves before being encoded. [IGNORE FOR CLAP]",
     )
     windowed.add_argument(
         "--window-overlap-percentage",
         type=float,
         default=0.0,
-        help="If enabled, the percentage each window should overlap into each other. Default no overlap.",
+        help="If enabled, the percentage each window should overlap into each other. Default no overlap. [IGNORE FOR CLAP]",
     )
 
     return parser
